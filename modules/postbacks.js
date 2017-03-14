8aef40771657495ee04445b78e88710964688a33
jkotak <kotakj@gmail.com>
2017-03-13 20:39:28 -0700

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -11,7 +11,7 @@ exports.schedule_visit = (sender, values) => {
 };
 
 exports.contact_broker = (sender, values) => {
-    messenger.send({text: "Here is the broker information for this property"}, sender);
+    messenger.send({text: "Here is the realtor information for this property"}, sender);
     messenger.send(formatter.formatBroker(), sender);
 };
 
@@ -23,8 +23,8 @@ exports.contact_me = (sender, values) => {
 
     let propertyId = values[1];
     messenger.getUserInfo(sender).then(response => {
-        salesforce.createCase(propertyId, response.first_name + " " + response.first_name, sender).then(() => {
-            messenger.send({text: `Thanks for your interest, ${response.first_name}. I asked a broker to contact you asap.`}, sender);
+        salesforce.createLead(propertyId, response.first_name + " " + response.first_name, sender).then(() => {
+            messenger.send({text: `Thanks for your interest, ${response.first_name}. I asked a realtor to contact you asap.`}, sender);
         });
     });
 
