9d1a94fd1f63d0435d455db7d010986535b7760e
jkotak <kotakj@gmail.com>
2017-03-14 18:33:33 -0700

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -144,10 +144,34 @@ let createCase = (propertyId, customerName, customerId) => {
 
 };
 
+let createLead = (propertyId, customerName, customerId) => {
+
+    return new Promise((resolve, reject) => {
+         let c = nforce.createSObject('Lead');
+        c.set('lastname', `Contact ${customerName} (Facebook Customer)`);
+        c.set('description', "Facebook id: " + customerId);
+        c.set('LeadSource', 'Facebook Bot');
+        c.set('status', 'New');
+        c.set('Property__c', propertyId);
+
+        org.insert({sobject: c}, err => {
+            if (err) {
+                console.error(err);
+                reject("An error occurred while creating a case");
+            } else {
+                resolve(c);
+            }
+        });
+    });
+
+};
+
+
 login();
 
 exports.org = org;
 exports.findProperties = findProperties;
 exports.findPropertiesByCategory = findPropertiesByCategory;
 exports.findPriceChanges = findPriceChanges;
-exports.createLead - createLead;
+exports.createLead = createLead;
+exports.createCase = createCase;
