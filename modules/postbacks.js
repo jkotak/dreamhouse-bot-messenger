4a2af55769b63cb9ce113001b1f849ca2f1ee6c2
jkotak <kotakj@gmail.com>
2017-03-14 19:03:07 -0700

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -23,8 +23,8 @@ exports.contact_me = (sender, values) => {
 
     let propertyId = values[1];
     messenger.getUserInfo(sender).then(response => {
-        salesforce.createLead(propertyId, response.first_name, response.first_name, sender).then(() => {
-            messenger.send({text: `Thanks for your interest, ${response.first_name}. I asked the listing agent to contact you asap.`}, sender);
+        salesforce.createLead(propertyId, response.first_name, response.last_name, sender).then(() => {
+            messenger.send({text: `Thanks for your interest, ${response.last_name}. I asked the listing agent to contact you asap.`}, sender);
         });
     });
 
