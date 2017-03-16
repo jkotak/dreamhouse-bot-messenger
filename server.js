84e98ab70761e32ea2ba20484b45be8661239844
jkotak <kotakj@gmail.com>
2017-03-15 23:23:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ app.post('/webhook', (req, res) => {
                     handler(sender, result.match);
                 } else {
                     console.log("Handler " + result.handlerName + " is not defined. Calling catch all function.");
-                    handler(sender,'catchall');
+                    catchall(sender,'catchall');
                 }
             }else {
                     console.log("Command is not defined. Calling catch all function.");
-                    handler(sender,'catchall');
+                    catchall(sender,'catchall');
             }
         } else if (event.postback) {
             let payload = event.postback.payload.split(",");
