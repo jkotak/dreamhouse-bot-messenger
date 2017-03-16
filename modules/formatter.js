62f8af5c3567eafa2586de87de5d043e66359f02
jkotak <kotakj@gmail.com>
2017-03-15 22:33:13 -0700

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -5,10 +5,6 @@ let moment = require("moment"),
 
 exports.formatProperties = properties => {
     let elements = [];
-    console.log('Length of this properties object'+properties.length);
-    if(properties.length==0){
-        console.log('Empty properties');
-    }
     properties.forEach(property => {
             elements.push({
                 title: property.get("Title__c"),
