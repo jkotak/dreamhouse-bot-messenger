7b66149fe3575fd1874efd62b7dfe6aba8984205
jkotak <kotakj@gmail.com>
2017-03-15 23:05:53 -0700

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -7,10 +7,7 @@ let salesforce = require('./salesforce'),
 exports.searchHouse = (sender) => {
     messenger.send({text: `OK, looking for houses for sale around you...`}, sender);
     salesforce.findProperties().then(properties => {
-        if(properties.length==0)
-            messenger.send({text: `Sorry, we couldn't find any houses around you...`}, sender);
-        else
-            messenger.send(formatter.formatProperties(properties), sender);
+        messenger.send(formatter.formatProperties(properties), sender);
     });
 };
 
@@ -24,20 +21,14 @@ exports.searchHouse_City = (sender, values) => {
 exports.searchHouse_Bedrooms_City_Range = (sender, values) => {
     messenger.send({text: `OK, looking for ${values[1]} bedrooms in ${values[2]} between ${values[3]} and ${values[4]}`}, sender);
     salesforce.findProperties({bedrooms: values[1], city: values[2]}).then(properties => {
-        if(properties.length==0)
-            messenger.send({text: `Sorry, no houses found for ${values[1]} bedrooms in ${values[2]} between ${values[3]} and ${values[4]}`}, sender);
-        else
-            messenger.send(formatter.formatProperties(properties), sender);
+        messenger.send(formatter.formatProperties(properties), sender);
     });
 };
 
 exports.searchHouse_Bedrooms_City = (sender, values) => {
     messenger.send({text: `OK, looking for ${values[1]} bedroom houses in ${values[2]}`}, sender);
     salesforce.findProperties({bedrooms: values[1], city: values[2]}).then(properties => {
-        if(properties.length==0)
-            messenger.send({text: `Sorry, no houses found for ${values[1]} bedroom houses in ${values[2]}`}, sender);
-        else
-            messenger.send(formatter.formatProperties(properties), sender);
+        messenger.send(formatter.formatProperties(properties), sender);
     });
 };
 
@@ -51,10 +42,7 @@ exports.searchHouse_Bedrooms = (sender, values) => {
 exports.searchHouse_Range = (sender, values) => {
     messenger.send({text: `OK, looking for houses between ${values[1]} and ${values[2]}`}, sender);
     salesforce.findProperties({priceMin: values[1], priceMax: values[2]}).then(properties => {
-        if(properties.length==0)
-            messenger.send({text: `Sorry, no houses found between ${values[1]} and ${values[2]}`}, sender);
-        else
-            messenger.send(formatter.formatProperties(properties), sender);
+        messenger.send(formatter.formatProperties(properties), sender);
     });
 };
 
