15b9bc81f9669e289680f16e2257e69ca41f8f21
jkotak <kotakj@gmail.com>
2017-03-16 22:52:43 -0700

    Update vision-service-mock.js

--- modules/vision-service-mock.js
+++ modules/vision-service-mock.js
@@ -5,3 +5,28 @@ exports.classify = imageURL => new Promise((resolve, reject) => {
         resolve("contemporary");
     }, 2000);
 });
+
+exports.address = (latitude, longitude) => {
+    return new Promise(function (resolve, reject) {
+        var request = new XMLHttpRequest();
+
+        var method = 'GET';
+        var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
+        var async = true;
+
+        request.open(method, url, async);
+        request.onreadystatechange = function () {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    var data = JSON.parse(request.responseText);
+                    var address = data.results[0];
+                    resolve(address);
+                }
+                else {
+                    reject(request.status);
+                }
+            }
+        };
+        request.send();
+    });
+};
