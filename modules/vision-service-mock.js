9d9040ebe87a682c56fad45c3661db137d305716
jkotak <kotakj@gmail.com>
2017-03-16 23:58:36 -0700

    Update vision-service-mock.js

--- modules/vision-service-mock.js
+++ modules/vision-service-mock.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var cities = require("cities")
+
 exports.classify = imageURL => new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("contemporary");
@@ -8,25 +10,6 @@ exports.classify = imageURL => new Promise((resolve, reject) => {
 
 exports.address = (latitude, longitude) => {
     return new Promise(function (resolve, reject) {
-        var request = new XMLHttpRequest();
-
-        var method = 'GET';
-        var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
-        var async = true;
-
-        request.open(method, url, async);
-        request.onreadystatechange = function () {
-            if (request.readyState == 4) {
-                if (request.status == 200) {
-                    var data = JSON.parse(request.responseText);
-                    var address = data.results[0];
-                    resolve(address);
-                }
-                else {
-                    reject(request.status);
-                }
-            }
-        };
-        request.send();
+       cities.gps_lookup(37.790293414181, 37.790293414181);
     });
 };
