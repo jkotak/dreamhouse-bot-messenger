da3b535efc8afa814b83299b3180cdd00c5ba15f
jkotak <kotakj@gmail.com>
2017-03-29 12:58:02 -0700

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -164,21 +164,24 @@ exports.formatPriceChanges = priceChanges => {
 exports.formatTransferAgent =()=>{
     return {
         "message":{
-        "text":"Do you want me to transfer you to a live agent?",
-        "quick_replies":[
-          {
-            "content_type":"text",
-            "title":"Yes",
-            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
-          },
-          {
-            "content_type":"text",
-            "title":"No",
-            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
-          }
-        ]
+            "text":"Do you want me to transfer you to a live agent?",
+            "attachment":{
+                "type":"text"
+            },
+            "quick_replies":[
+              {
+                "content_type":"text",
+                "title":"Yes",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+              },
+              {
+                "content_type":"text",
+                "title":"No",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+              }
+            ]
+        }
     }
-  }
 }
 
 exports.formatAppointment = property => {
