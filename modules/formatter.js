1ecb0b818554aa574b43fe10f24c97f5b76a0919
jkotak <kotakj@gmail.com>
2017-03-31 10:15:56 -0700

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -134,7 +134,7 @@ exports.formatLoanAccountLinking = () =>{
                 "buttons": [
                     {  
                     "type":"account_link",
-                    "url":"https://mortgagecommunitydemo-15801ffe7f4.force.com/realtors/FBCommunitiesLoginPage"
+                    "url":"https://mortgagecommunitydemo-15801ffe7f4.force.com/realtors/FBCommunitiesLoginPage?startURL=FBRedirect"
                     }
                 ]
             }
