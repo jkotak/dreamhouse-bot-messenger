4020d5cf1b972db1a86a8e07743c660dfe4478d1
jkotak <kotakj@gmail.com>
2017-03-31 13:39:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.get('/authorize', (req, res) => {
 
 app.post('/webhook', (req, res) => {
     let events = req.body.entry[0].messaging;
+    console.log(events);
+    console.log(req);
     for (let i = 0; i < events.length; i++) {
         let event = events[i];
         let sender = event.sender.id;
