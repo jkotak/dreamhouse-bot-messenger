49bfec51acd47c236094356aa939d729f1f0b7b3
jkotak <kotakj@gmail.com>
2017-06-02 17:13:51 -0700

    Update messenger.js

--- modules/messenger.js
+++ modules/messenger.js
@@ -20,6 +20,52 @@ exports.send = (message, recipient) => {
         }
     });
 };
+exports.setMenu = (message, recipient) => {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: FB_PAGE_TOKEN},
+        method: 'POST',
+        json: {
+            "persistent_menu":[
+                {
+                  "locale":"default",
+                  "composer_input_disabled":true,
+                  "call_to_actions":[
+                    {
+                      "title":"My Account",
+                      "type":"nested",
+                      "call_to_actions":[
+                        {
+                          "title":"Pay Bill",
+                          "type":"postback",
+                          "payload":"PAYBILL_PAYLOAD"
+                        },
+                        {
+                          "title":"My Loans",
+                          "type":"postback",
+                          "payload":"HISTORY_PAYLOAD"
+                        },
+                        {
+                          "title":"Loan Status",
+                          "type":"postback",
+                          "payload":"CONTACT_INFO_PAYLOAD"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+       }
+    }, (error, response) => {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+    
 
 exports.getUserInfo = (userId) => {
 
