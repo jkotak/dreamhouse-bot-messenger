8044fc49680592c0a4b5b90028d6d53aa8c043f6
jkotak <kotakj@gmail.com>
2017-06-08 11:01:20 -0700

    Create UserInfo.js

---
+++ models/UserInfo.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserInfoSchema = new Schema({
+  user_id: {type: String},
+  email: {type: String},
+  phone: {type: String},
+  last_keyword:{type:String}
+});
+
+module.exports = mongoose.model("UserInfo", UserInfoSchema);
