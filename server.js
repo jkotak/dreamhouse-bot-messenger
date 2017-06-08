cea75d6d178c929d413fbc64896bcf132fc9a1a9
jkotak <kotakj@gmail.com>
2017-06-08 10:32:47 -0700

    Create server.js

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ var express = require('express'),
     handlers = require('./modules/handlers'),
     postbacks = require('./modules/postbacks'),
     uploads = require('./modules/uploads'),
+    mongoose = require("mongoose"),
     menu = require('./modules/menu'),
     FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN,
     app = express();
+
+
+var db = mongoose.connect(process.env.MONGODB_URI);
+var Movie = require("./models/loanapplication");
 
 var stopbot = false;
 var isMenuSet = false;
@@ -37,62 +42,64 @@ app.get('/authorize', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-    if(!isMenuSet){
-        menu.createMenu();
-        isMenuSet = true;
-    }
-    let events = req.body.entry[0].messaging;
-    console.log('User ID:' + this.userid);
-    for (let i = 0; i < events.length; i++) {
-        let event = events[i];
-        let sender = event.sender.id;
-        if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
-            sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
-        } else if (event.message && event.message.text && !stopbot) {
-            let result = processor.match(event.message.text);
-            if (result) {
-                let handler = handlers[result.handler];
-                if (handler && typeof handler === "function") {
-                    handler(sender, result.match);
-                } else {
-                    console.log("Handler " + result.handlerName + " is not defined. Calling catch all function.");
-                    handlers.catchall(sender);
+    if (req.body.object == "page") {
+        if(!isMenuSet){
+            menu.createMenu();
+            isMenuSet = true;
+        }
+        let events = req.body.entry[0].messaging;
+        console.log('User ID:' + this.userid);
+        for (let i = 0; i < events.length; i++) {
+            let event = events[i];
+            let sender = event.sender.id;
+            if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
+                sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+            } else if (event.message && event.message.text && !stopbot) {
+                let result = processor.match(event.message.text);
+                if (result) {
+                    let handler = handlers[result.handler];
+                    if (handler && typeof handler === "function") {
+                        handler(sender, result.match);
+                    } else {
+                        console.log("Handler " + result.handlerName + " is not defined. Calling catch all function.");
+                        handlers.catchall(sender);
+                    }
+                }else if(event.message.text=='Transfer me'){
+                    console.log("Asked for Agent");
+                    stopbot = true;
+                    handlers.ContinueWithAgent(sender);
+                }else if(event.message.text=='Continue with bot'){
+                    console.log("Asked for Agent");
+                    handlers.ContinueWithoutAgent(sender);
+                }else {
+                        console.log("Command" + event.message.text +" is not defined. Calling catch all function. Event.Message" + event.message);
+                        handlers.catchall(sender);
                 }
-            }else if(event.message.text=='Transfer me'){
-                console.log("Asked for Agent");
-                stopbot = true;
-                handlers.ContinueWithAgent(sender);
-            }else if(event.message.text=='Continue with bot'){
-                console.log("Asked for Agent");
-                handlers.ContinueWithoutAgent(sender);
-            }else {
-                    console.log("Command" + event.message.text +" is not defined. Calling catch all function. Event.Message" + event.message);
-                    handlers.catchall(sender);
-            }
-        }else if(event.message && event.message.text && stopbot){
-            if(event.message.text.toLowerCase()=='wakeup'){
-                stopbot = false;
-                handlers.wakeup(sender);
-            }
-        }else if(event.account_linking){
-            console.log('status'+event.account_linking.status);
-            handlers.authenticated(sender,event.account_linking.authorization_code);
-        }else if (event.postback) {
-            console.log('Payload:'+ event.postback.payload);
-            let payload = event.postback.payload.split(",");
-            console.log('Payload:'+ payload);
-            let postback = postbacks[payload[0]];
-            if (postback && typeof postback === "function") {
-                postback(sender, payload);
-            } else {
-                console.log("Postback " + postback + " is not defined");
-            }
-        } else if (event.message && event.message.attachments) {
-            uploads.processUpload(sender, event.message.attachments);
-        } 
-    }
+            }else if(event.message && event.message.text && stopbot){
+                if(event.message.text.toLowerCase()=='wakeup'){
+                    stopbot = false;
+                    handlers.wakeup(sender);
+                }
+            }else if(event.account_linking){
+                console.log('status'+event.account_linking.status);
+                handlers.authenticated(sender,event.account_linking.authorization_code);
+            }else if (event.postback) {
+                console.log('Payload:'+ event.postback.payload);
+                let payload = event.postback.payload.split(",");
+                console.log('Payload:'+ payload);
+                let postback = postbacks[payload[0]];
+                if (postback && typeof postback === "function") {
+                    postback(sender, payload);
+                } else {
+                    console.log("Postback " + postback + " is not defined");
+                }
+            } else if (event.message && event.message.attachments) {
+                uploads.processUpload(sender, event.message.attachments);
+            } 
+        }
     
-    res.sendStatus(200);
+        res.sendStatus(200);
+    }
 });
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
