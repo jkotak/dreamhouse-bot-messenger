f63542f5f1a13cad4a1c92057c9bce95888491bf
jkotak <kotakj@gmail.com>
2017-06-08 20:51:57 -0700

    Create loanapplication.js

--- models/loanapplication.js
+++ models/loanapplication.js
@@ -11,23 +11,4 @@ var LoanApplicationSchema = new Schema({
   property_type: {type: String}
 });
 
-
-let createLoanApp = (userid,update) => {
-    coonsole.log(userid + ' ' + update);
-    var query = {user_id: userid};
-    var options = {upsert: true, returnNewDocument : true};
-    return new Promise((resolve, reject) => {
-        UserInfo.findOneAndUpdate(query, update, options, function(err, user) {
-            if (err) {
-                 reject("An error as occurred");
-            } else {
-                resolve(user);
-            }
-        });
-    });
-};
-
-
-
 module.exports = mongoose.model("LoanApplication", LoanApplicationSchema);
-exports.createLoanApp = createLoanApp;
