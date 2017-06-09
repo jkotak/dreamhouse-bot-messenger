644a4573efe4f022c7276000575e03f2f8c0f4b6
jkotak <kotakj@gmail.com>
2017-06-08 22:34:02 -0700

    Create handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -98,8 +98,8 @@ exports.authenticated =(sender,userid)=>{
 }
 
 exports.startApplication = (sender,userinfo) =>{
-    messenger.getUserInfo(sender).then(response => {
-        messenger.send({text: `${response.first_name}, happy to help. I will walk you through a series of questions to gather some information. Once we complete these steps, I can send you a preapproval form.`}, sender).then(()=>{
+    messenger.getUserInfo(sender).then((response,userinfo) => {
+        messenger.send({text: `${response.first_name}, happy to help. I will walk you through a series of questions to gather some information. Once we complete these steps, I can send you a preapproval form.`}, sender).then((userinfo)=>{
              var update = {
                         user_id: userinfo.user_id
                     };
