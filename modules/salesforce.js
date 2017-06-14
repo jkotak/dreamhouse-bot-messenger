717f85515d32f031ef5d5e4ebdce48c28488e26c
jkotak <kotakj@gmail.com>
2017-06-13 23:35:23 -0700

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -266,7 +266,7 @@ let createLoanApp = (fileURL, fileName, fileType,salesforce_lead_id) => {
 
         res.on('end', function(){
             return new Promise((resolve, reject) => {
-                var base64data = new Buffer(imagedata,'binary').toString('base64');
+                var base64data = new Buffer(imagedata,'binary');
                 console.log(base64data);
                 console.log('res.headers["content-type"] ' + res.headers["content-type"] );
                 var c = nforce.createSObject('Attachment', {
