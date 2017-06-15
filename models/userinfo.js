8f3f94bd7d1f66de25e10965935fdc8a27cde760
jkotak <kotakj@gmail.com>
2017-06-14 20:50:35 -0700

    Create userinfo.js

--- models/userinfo.js
+++ models/userinfo.js
@@ -5,7 +5,8 @@ var UserInfoSchema = new Schema({
   user_id: {type: String},
   email_address: {type: String},
   phone_number: {type: String},
-  last_keyword:{type:String}
+  last_keyword:{type:String},
+  stop_bot:{type:Boolean}
 });
 
 module.exports = mongoose.model("UserInfo", UserInfoSchema);
