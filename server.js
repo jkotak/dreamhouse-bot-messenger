ea07865d9855802b08e7b20d23006a3ab3308ccd
jkotak <kotakj@gmail.com>
2017-06-14 21:31:09 -0700

    Create server.js

--- server.js
+++ server.js
@@ -52,74 +52,76 @@ app.post('/webhook', (req, res) => {
         for (let i = 0; i < events.length; i++) {
             let event = events[i];
             let sender = event.sender.id;
-            if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
-                sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
-            } else if (event.message && event.message.text) {
-                let result = processor.match(event.message.text);
-                if (result) {
-                    let handler = handlers[result.handler];
-                    userinfohandler.getSetUserHistory(sender,result.handler).then(user => {
-                        console.log('handler:'+ result.handler);
-                        if (handler && typeof handler === "function") {
-                            if(result.handler==='startApplication'){
-                                handler(sender, user,['startApplication','askFirstQuestion']);
-                            }else{
-                                handler(sender, result.match);
-                            }
-                        } else {
-                            console.log("Handler " + result.handlerName + " is not defined. Calling catch all function.");
-                            handlers.catchall(sender);
-                        }
-                    });
-                }else {
-                    userinfohandler.getUserHistory(sender).then(user => {
-                        console.log(typeof event.message.quick_reply);
-                        if(user.last_keyword !== null &&  typeof user.last_keyword !== 'undefined' && user.last_keyword==='startApplication'){
-                            if (event.message.quick_reply !== null && typeof event.message.quick_reply === 'object'){
-                                var payload = event.message.quick_reply.payload; 
-                                console.log('Quick Reply'+ payload);
-                                var params = payload.split(",");
-                                let handler = handlers[params[0]];
-                                if (handler && typeof handler === "function") {
-                                    handler(sender, user,params);
+            userinfohandler.updateUserInfo(sender,{}).then(user => {
+                if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
+                    sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+                } else if (event.message && event.message.text) {
+                    let result = processor.match(event.message.text);
+                    if (result) {
+                        let handler = handlers[result.handler];
+                        userinfohandler.getSetUserHistory(sender,result.handler).then(user => {
+                            console.log('handler:'+ result.handler);
+                            if (handler && typeof handler === "function") {
+                                if(result.handler==='startApplication'){
+                                    handler(sender, user,['startApplication','askFirstQuestion']);
+                                }else{
+                                    handler(sender, result.match);
                                 }
-                            }else if (validator.isEmail(event.message.text)){
-                                 let handler = handlers[user.last_keyword];
-                                 handler(sender, user,['startApplication','askFifthQuestion','email',event.message.text]);
-                            }else if (phoneregex({ exact: true }).test(event.message.text)){
-                                 let handler = handlers[user.last_keyword];
-                                 handler(sender, user,['startApplication','askSixthQuestion','phone',event.message.text]);
-                            }else if(validator.isCurrency(event.message.text)){
-                                let handler = handlers[user.last_keyword];
-                                 handler(sender, user,['startApplication','askFourthQuestion','amount',numeral(event.message.text).value()]);
-                            }else{
-                                let handler = handlers[user.last_keyword];
-                                console.log("Command" + event.message.text +" is not defined. Calling catch all function. Event.Message" + event.message);
-                                handler(sender, user,['startApplication','Error']);
+                            } else {
+                                console.log("Handler " + result.handlerName + " is not defined. Calling catch all function.");
+                                handlers.catchall(sender);
                             }
-                        }else{
-                            console.log("Command" + event.message.text +" is not defined. Calling catch all function. Event.Message" + event.message);
-                            handlers.catchall(sender); 
-                        }
-                    });
-                }
-            }else if(event.account_linking){
-                console.log('status'+event.account_linking.status);
-                handlers.authenticated(sender,event.account_linking.authorization_code);
-            }else if (event.postback) {
-                console.log('Payload:'+ event.postback.payload);
-                let payload = event.postback.payload.split(",");
-                console.log('Payload:'+ payload);
-                let postback = postbacks[payload[0]];
-                if (postback && typeof postback === "function") {
-                    postback(sender, payload);
-                } else {
-                    console.log("Postback " + postback + " is not defined");
-                }
-            } else if (event.message && event.message.attachments) {
-                uploads.processUpload(sender, event.message.attachments);
-            } 
-        }
+                        });
+                    }else {
+                        userinfohandler.getUserHistory(sender).then(user => {
+                            console.log(typeof event.message.quick_reply);
+                            if(user.last_keyword !== null &&  typeof user.last_keyword !== 'undefined' && user.last_keyword==='startApplication'){
+                                if (event.message.quick_reply !== null && typeof event.message.quick_reply === 'object'){
+                                    var payload = event.message.quick_reply.payload; 
+                                    console.log('Quick Reply'+ payload);
+                                    var params = payload.split(",");
+                                    let handler = handlers[params[0]];
+                                    if (handler && typeof handler === "function") {
+                                        handler(sender, user,params);
+                                    }
+                                }else if (validator.isEmail(event.message.text)){
+                                     let handler = handlers[user.last_keyword];
+                                     handler(sender, user,['startApplication','askFifthQuestion','email',event.message.text]);
+                                }else if (phoneregex({ exact: true }).test(event.message.text)){
+                                     let handler = handlers[user.last_keyword];
+                                     handler(sender, user,['startApplication','askSixthQuestion','phone',event.message.text]);
+                                }else if(validator.isCurrency(event.message.text)){
+                                    let handler = handlers[user.last_keyword];
+                                     handler(sender, user,['startApplication','askFourthQuestion','amount',numeral(event.message.text).value()]);
+                                }else{
+                                    let handler = handlers[user.last_keyword];
+                                    console.log("Command" + event.message.text +" is not defined. Calling catch all function. Event.Message" + event.message);
+                                    handler(sender, user,['startApplication','Error']);
+                                }
+                            }else{
+                                console.log("Command" + event.message.text +" is not defined. Calling catch all function. Event.Message" + event.message);
+                                handlers.catchall(sender); 
+                            }
+                        });
+                    }
+                }else if(event.account_linking){
+                    console.log('status'+event.account_linking.status);
+                    handlers.authenticated(sender,event.account_linking.authorization_code);
+                }else if (event.postback) {
+                    console.log('Payload:'+ event.postback.payload);
+                    let payload = event.postback.payload.split(",");
+                    console.log('Payload:'+ payload);
+                    let postback = postbacks[payload[0]];
+                    if (postback && typeof postback === "function") {
+                        postback(sender, payload);
+                    } else {
+                        console.log("Postback " + postback + " is not defined");
+                    }
+                } else if (event.message && event.message.attachments) {
+                    uploads.processUpload(sender, event.message.attachments);
+                } 
+            });//end find user
+        }//end loop
     
         res.sendStatus(200);
     }
