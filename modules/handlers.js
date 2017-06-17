5f8e8377894e25000865beadc9be020a95d00220
jkotak <kotakj@gmail.com>
2017-06-16 19:37:48 -0700

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -121,9 +121,9 @@ exports.startCase = (sender,params) =>{
                 current_stage = thiscase.current_stage+2;
            }
            if(current_stage==4){
-               moreparams[casehandler.getFieldName(0)]=thiscase[casehandler.getFieldName(0)];
-               moreparams[casehandler.getFieldName(1)]=thiscase[casehandler.getFieldName(1)];
-               moreparams[casehandler.getFieldName(2)]=thiscase[casehandler.getFieldName(2)];
+               moreparams["$1$"]=thiscase[casehandler.getFieldName(0)];
+               moreparams["$2$"]=thiscase[casehandler.getFieldName(1)];
+               moreparams["$3$"]=thiscase[casehandler.getFieldName(2)];
            }
         }else{
            update["current_stage"]=0;
