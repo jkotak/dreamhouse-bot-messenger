e22b9900a1d6252ec9c5dc939fb28a67dad2e29e
jkotak <kotakj@gmail.com>
2017-06-17 19:58:21 -0700

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -130,7 +130,7 @@ exports.startCase = (sender,params) =>{
         }
         console.log('casehandler.isTheEnd(current_stage):'+casehandler.isTheEnd(current_stage));
         if(casehandler.isTheEnd(current_stage)){
-            casehandler.deleteCase (sender).then((thiscase) { 
+            casehandler.deleteCase (sender).then((thiscase) =>{ 
                 userinfohandler.getSetUserHistory(sender,"help").then(() => {
                     messenger.send(casehandler.createQuestion(sender,current_stage,params[0],moreparams), sender);
                 });
