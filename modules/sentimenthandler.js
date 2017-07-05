7e3d3396c366f9b3da711d3470929820f02fe160
jkotak <kotakj@gmail.com>
2017-07-05 07:53:11 -0700

    Update and rename sentimenthandler to sentimenthandler.js

---
+++ modules/sentimenthandler.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var EINSTEIN_PRIVATE_KEY = process.env.EINSTEIN_PRIVATE_KEY,
+    EINSTEIN_USERNAME = process.env.EINSTEIN_USERNAME,
+    EINSTEIN_URL = process.env.EINSTEIN_URL
+
+
+exports.getToken = () => {
+    
+};
