1ef768a704162801fc2f698d3256a647a90a00b4
jkotak <kotakj@gmail.com>
2017-07-11 21:04:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -130,8 +130,7 @@ app.post('/webhook', (req, res) => {
 });
 
 
-updateToken(pvsUrl, accountId, privateKey).then(() => {
+
     app.listen(app.get('port'), function () {
         console.log('Express server listening on port ' + app.get('port'));
     });
-};
