ae81b2743fe2701b5954fcbf12ef4dbd0de9d249
jkotak <kotakj@gmail.com>
2017-07-11 19:50:02 -0700

    Update sentimenthandler.js

--- modules/sentimenthandler.js
+++ modules/sentimenthandler.js
@@ -1,10 +1,40 @@
 "use strict";
 
+
+var request = require('request');
+
 var EINSTEIN_PRIVATE_KEY = process.env.EINSTEIN_PRIVATE_KEY,
     EINSTEIN_USERNAME = process.env.EINSTEIN_USERNAME,
     EINSTEIN_URL = process.env.EINSTEIN_URL
 
 
 exports.getToken = () => {
-    
+    request({
+      url: 'https://api.einstein.ai/v1/oauth2/token',
+      method: 'POST',
+      auth: {
+        user: EINSTEIN_USERNAME,
+        pass: 'yyy'
+      },
+      form: {
+        'grant_type': 'client_credentials'
+      }
+    }, function(err, res) {
+      var json = JSON.parse(res.body);
+      console.log("Access Token:", json.access_token);
+    });
 };
+
+exports.getSentiment = () => {
+    request({
+      url: 'https://api.someapi.com/blah/something',
+      auth: {
+        'bearer': accessToken
+      }
+    }, function(err, res) {
+      console.log(res.body);
+    });
+
+}
+ 
+        
