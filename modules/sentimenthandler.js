ea0a22a20b487aa5f1e8d72f1a6401dda9d46f31
jkotak <kotakj@gmail.com>
2017-07-12 09:00:55 -0700

    Update sentimenthandler.js

--- modules/sentimenthandler.js
+++ modules/sentimenthandler.js
@@ -1,9 +1,6 @@
 "use strict";
 
-
-
-
-exports.getSentiment = () => {
+/*exports.getSentiment = () => {
     request({
       url: 'https://api.someapi.com/blah/something',
       auth: {
@@ -13,6 +10,74 @@ exports.getSentiment = () => {
       console.log(res.body);
     });
 
+}*/
+
+const rp = require('request-promise');
+var Episode7 = require('episode-7');
+const updateToken = require('./update-token');
+const oAuthToken = require('./oauth-token');
+
+let loopPreventor = false;
+
+function* querySentimentApi(
+                       pvsUrl,
+                       document,
+                       modelId='CommunitySentiment',
+                       accountId,
+                       privateKey,
+                       jwtToken){
+  var token = jwtToken || oAuthToken.get();
+
+  var formData = {
+    modelId: modelId,
+    document : document
+  }
+  var options = {
+      url: `${pvsUrl}v2/language/sentiment`,
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+
+      },
+      formData:formData
+  }
+  let { body, isUnauthorized } = yield Episode7.call((options) => {
+    return rp(options)
+      .then( body => ({ body }) )
+      .catch( error => {
+        if(error.statusCode === 401) {
+          return { isUnauthorized: true };
+        } else {
+          throw error;
+        }
+      })
+  },options);
+  
+  if(!loopPreventor && isUnauthorized) {
+    loopPreventor = true;
+    let updatedToken = yield Episode7.call(
+      updateToken,
+      pvsUrl,
+      accountId,
+      privateKey
+    );
+
+    let sentimentApiResult = yield Episode7.call( 
+      querySentimentApi,
+      pvsUrl,
+      resizedImgUrl,
+      modelId
+    );
+    setTimeout(()=>{loopPreventor = false},1000);
+    return sentimentApiResult;
+  } else {
+    return body;
+  }
 }
+
+module.exports = querySentimentApi;
  
         
