f2acb857d39b1a765fcad8a1d906d991f53d2f5e
jkotak <kotakj@gmail.com>
2017-07-13 23:26:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post('/webhook', (req, res) => {
                 privateKey,
                 jwtToken
               ).then(predictions=>{
-                console.log('Printing'+ predictions.probabilities);
+                console.log('Printing'+ predictions);
               });
             userinfohandler.findOneAndUpdateUserInfo(sender,{}).then(user => {
                 if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
