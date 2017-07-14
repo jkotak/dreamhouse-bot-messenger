90a8cb3f617858f2115e170395a84fbecc3d4bb1
jkotak <kotakj@gmail.com>
2017-07-13 23:12:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -58,10 +58,10 @@ app.post('/webhook', (req, res) => {
             isMenuSet = true;
         }
         let events = req.body.entry[0].messaging;
-        console.log('User ID:' + this.userid);
         for (let i = 0; i < events.length; i++) {
             let event = events[i];
             let sender = event.sender.id;
+            var sentiment;
             let t = Episode7.run(
                 querySentimentApi,
                 pvsUrl,
@@ -70,9 +70,8 @@ app.post('/webhook', (req, res) => {
                 accountId,
                 privateKey,
                 jwtToken
-              ).then(prediction =>{
-                console.log('Predictions are'+ prediction);
-              });
+              );
+            console.log('Printing'+ t.next);
             userinfohandler.findOneAndUpdateUserInfo(sender,{}).then(user => {
                 if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
                     sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
