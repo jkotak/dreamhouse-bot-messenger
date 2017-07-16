161b02516a2659dd59d44525ad41a3165a3d4e11
jkotak <kotakj@gmail.com>
2017-07-15 18:44:28 -0700

    Update userinfo.js

--- models/userinfo.js
+++ models/userinfo.js
@@ -7,7 +7,9 @@ var UserInfoSchema = new Schema({
   email_address: {type: String},
   phone_number: {type: String},
   last_keyword:{type:String},
-  stop_bot:{type:Boolean}
+  stop_bot:{type:Boolean},
+  first_name: {type: String},
+  last_name:{type:String}
 });
 
 module.exports = mongoose.model("UserInfo", UserInfoSchema);
