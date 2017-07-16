34d584622ba3cea84040129d6e6ca1f6b1ceb81e
jkotak <kotakj@gmail.com>
2017-07-15 20:30:52 -0700

    Update uploads.js

--- modules/uploads.js
+++ modules/uploads.js
@@ -6,33 +6,37 @@ let messenger = require('./messenger'),
     loanapplicationhandler = require('./loanapplicationhandler'),
     visionService = require('./vision-service-mock');
 
-exports.processUpload = (sender, attachments) => {
+var apps = ["startcase","startApplication"];
+
+
+exports.processUpload = (sender, attachments,lastKeyword) => {
     
     if (attachments.length > 0) {
         let attachment = attachments[0];
         if (attachment.type === "image") {
-            loanapplicationhandler.findLoanApp(sender).then(loanApp => {
-                console.log('Sending URL'+attachment.payload.url);
-                console.log('Attachment' + attachment.payload);
-                if (loanApp && "process_docs"===loanApp.current_state) {
-                     salesforce.createLoanApp(attachment.payload.url,'driver_license',attachment.type,loanApp.salesforce_lead_id,function(err,result){
-                        if ( err ) {
-                            // handle the error safely
-                            console.log('Error is', err);
+            if(apps.indexOf(lastKeyword.toLowerCase()) > -1){
+                if(lastKeyword==="startApplication"){
+                    loanapplicationhandler.findLoanApp(sender).then(loanApp => {
+                        if (loanApp && "process_docs"===loanApp.current_state) {
+                             salesforce.createLoanApp(attachment.payload.url,'driver_license',attachment.type,loanApp.salesforce_lead_id,function(err,result){
+                                if ( err ) {
+                                    // handle the error safely
+                                    console.log('Error is', err);
+                                }
+                             });
+                             messenger.send(loanapplicationhandler.processLoanApplicationConfirmation(), sender);
                         }
-                     });
-                     messenger.send(loanapplicationhandler.processLoanApplicationConfirmation(), sender);
-                }else{
-                    messenger.send({text: 'OK, let me look at that picture...'}, sender);
-                    visionService.classify(attachment.url)
-                        .then(houseType => {
-                            messenger.send({text: `Looking for houses matching "${houseType}"`}, sender);
-                            return salesforce.findPropertiesByCategory(houseType)
-                        })
-                    .then(properties => messenger.send(formatter.formatProperties(properties), sender))
+                    });
                 }
-            });
-            
+            }else{
+                messenger.send({text: 'OK, let me look at that picture...'}, sender);
+                visionService.classify(attachment.url)
+                    .then(houseType => {
+                        messenger.send({text: `Looking for houses matching "${houseType}"`}, sender);
+                        return salesforce.findPropertiesByCategory(houseType)
+                    })
+                .then(properties => messenger.send(formatter.formatProperties(properties), sender))
+            }
         }else if (attachment.type === "location") {
             visionService.address( attachment.payload.coordinates.lat, attachment.payload.coordinates.long)
                 .then(city => {
